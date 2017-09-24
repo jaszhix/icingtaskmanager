5b40075a246af63bcba0f3e3ed6c4d875da8da6d
Jason Hicks <jaszhix@gmail.com>
2017-09-24 16:28:09 -0500

    Update to 6.0.0
    Closes #150, #149, #146, and #133

--- IcingTaskManager@json/3.2/timers.js
+++ IcingTaskManager@json/3.2/timers.js
@@ -1,6 +1,6 @@
 const Mainloop = imports.mainloop;
 
-const setTimeout = function(func, ms /* , ... args */) {
+const setTimeout = function(func, ms) {
   let args = [];
   if (arguments.length > 2) {
     args = args.slice.call(arguments, 2);
@@ -18,7 +18,7 @@ const clearTimeout = function(id) {
   Mainloop.source_remove(id);
 };
 
-const setInterval = function(func, ms /* , ... args */) {
+const setInterval = function(func, ms) {
   let args = [];
   if (arguments.length > 2) {
     args = args.slice.call(arguments, 2);
@@ -35,3 +35,37 @@ const setInterval = function(func, ms /* , ... args */) {
 const clearInterval = function(id) {
   Mainloop.source_remove(id);
 };
+
+function throttle(fn, interval, callFirst) {
+  let wait = false;
+  let callNow = false;
+  return function() {
+    callNow = callFirst && !wait;
+    let context = this;
+    let args = arguments;
+    if (!wait) {
+      wait = true;
+      setTimeout(function() {
+        wait = false;
+        if (!callFirst) {
+          return fn.apply(context, args);
+        }
+      }, interval);
+    }
+    if (callNow) {
+      callNow = false;
+      return fn.apply(this, arguments);
+    }
+  };
+}
+
+function defer(func) {
+  return new Proxy(func, {
+    apply: function(target, boundObject, argumentsList) {
+      Mainloop.idle_add_full(Mainloop.PRIORITY_DEFAULT, function() {
+        target.apply(boundObject, argumentsList);
+      });
+      return true;
+    }
+  });
+}
